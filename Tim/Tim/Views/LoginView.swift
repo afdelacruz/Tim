d553dedf58619b40f82ca578189f15ab77e8f8db
Andrew De la Cruz <your.email@example.com>
2025-06-06 09:56:23 -0700

    feat(login): increase waving Tim size from 60x60 to 100x100 pixels

--- Tim/Tim/Views/LoginView.swift
+++ Tim/Tim/Views/LoginView.swift
@@ -91,7 +91,7 @@ struct LoginView: View {
                         Image("TimWaving")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(width: 60, height: 60)
+                            .frame(width: 100, height: 100)
                         Spacer()
                     }
                     .padding(.leading, TimSpacing.lg)
